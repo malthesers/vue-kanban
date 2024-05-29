d8e69dc5d1e0ce01050714374925028afc84e4f4
malt9830 <malt9830@stud.kea.dk>
2024-05-29 14:40:37 +0200

    Componentialised EditingShadow

--- src/components/TaskCard/TaskCardEditingShadow.vue
+++ src/components/TaskCard/TaskCardEditingShadow.vue
@@ -1,14 +1,5 @@
 <template>
-  <Teleport to="body">
-    <!-- <Transition name="fade"> -->
-    <VSheet
-      v-if="isEditing"
-      @click="emits('disableEditing')"
-      class="position-absolute top-0 left-0 w-screen h-screen bg-black opacity-70 cursor-pointer"
-    >
-    </VSheet>
-    <!-- </Transition> -->
-  </Teleport>
+  <EditingShadow :showShadow="isEditing" :hideShadow="() => emits('disableEditing')" />
 </template>
 
 <script setup lang="ts">
@@ -21,15 +12,5 @@ defineProps<{
 <style scoped>
 .v-sheet {
   z-index: 2000;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 300ms ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
 }
 </style>
---
+++ src/components/layout/EditingShadow.vue
@@ -0,0 +1,23 @@
+<template>
+  <Teleport to="body">
+    <VSheet
+      v-if="showShadow"
+      @click="hideShadow"
+      class="position-absolute top-0 left-0 w-screen h-screen bg-black opacity-70 cursor-pointer"
+    >
+    </VSheet>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  showShadow: boolean
+  hideShadow: () => void
+}>()
+</script>
+
+<style scoped>
+.v-sheet {
+  z-index: 2000;
+}
+</style>
