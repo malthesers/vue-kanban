016e99c7a1443262facdd90a7a1a8ac831cab20d
Malthe Kusk Lauritsen <malthekusk@gmail.com>
2024-08-05 16:15:37 +0200

    Added created_at column to getTasks SELECT

--- src/api/getTasks.ts
+++ src/api/getTasks.ts
@@ -3,10 +3,12 @@ import { supabase } from '@/lib/supabaseClient'
 export default async function getTasks() {
   const { data, error } = await supabase.from('containers').select(`
     id,
+    created_at,
     title,
     color,
     tasks (
       id,
+      created_at,
       title,
       description
     )
