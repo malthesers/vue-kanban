fd14f435959e1dc8924c670102711fe844281944
malt9830 <malt9830@stud.kea.dk>
2024-08-06 11:47:33 +0200

    Hidden header buttons while not signed in

--- src/components/AuthenticationPanel/AuthenticationPanel.vue
+++ src/components/AuthenticationPanel/AuthenticationPanel.vue
@@ -1,6 +1,6 @@
 <template>
   <VMain class="h-screen d-flex">
-    <VSheet class="w-100 mx-auto my-auto h-fit px-4 py-4" max-width="400"> <LoginForm /></VSheet>
+    <VSheet class="w-100 mx-auto my-auto h-fit px-4 py-4 rounded-lg" max-width="400"> <LoginForm /></VSheet>
   </VMain>
 </template>
 
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,11 +1,15 @@
 <template>
   <VAppBar :elevation="3" class="py-2 px-4">
     <VAppBarTitle class="font-weight-bold">Kanban Board</VAppBarTitle>
-    <VSheet class="d-flex ga-4">
+    <VSheet v-if="isLoggedIn" class="d-flex ga-4">
       <AddStatusContainerButton />
       <SignOutButton />
     </VSheet>
   </VAppBar>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore'
+
+const { isLoggedIn } = useAuthStore()
+</script>
